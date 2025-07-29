27b20b32d3c34458c7a8bd45861c0b9ce75a2388
Nupur Singh Gour <155764854+NupurGour654@users.noreply.github.com>
2025-07-29 20:40:37 +0530

    Add cleaning runner script

--- notebooks/03_data_cleaning.py
+++ notebooks/03_data_cleaning.py
@@ -1,1 +1,17 @@
 
+from load_data import load_csv
+from check_missing import check_missing
+
+files = [
+    "day_wise.csv",
+    "country_wise_latest.csv",
+    "covid_19_clean_complete.csv",
+    "full_grouped.csv",
+    "worldometer_data.csv"
+]
+
+for file in files:
+    df = load_csv(file)
+    check_missing(df, file)
+
+
